4afc093145bbefe3f7a293027956cc8d2e2e4461
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-02-07 09:36:13 +0100

    first commit

---
+++ ts/first-app.js
@@ -0,0 +1,6 @@
+var userName = "Blaise";
+// userName = 34;
+userName = "Max";
+var userAge = 34;
+var isInvalid = true;
+// string, number, boolean
--- ts/first-app.ts
+++ ts/first-app.ts
@@ -1,2 +1,9 @@
-let userName: string | number = "Blaise";
-userName = 34;
+let userName: string = "Blaise";
+// userName = 34;
+userName = "Max";
+
+let userAge: number = 34;
+
+let isInvalid: boolean = true;
+
+// string, number, boolean
