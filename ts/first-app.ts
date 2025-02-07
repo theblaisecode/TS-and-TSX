f898bab8ba017caa0929a9fc03fb4dfb81799ab9
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-02-07 09:45:52 +0100

    first commit

--- ts/first-app.ts
+++ ts/first-app.ts
@@ -1,9 +1,26 @@
+// ----------------------------------------------- String
 let userName: string = "Blaise";
 // userName = 34;
 userName = "Max";
 
+// ----------------------------------------------- Number
 let userAge: number = 34;
 
+// ----------------------------------------------- Boolean
 let isInvalid: boolean = true;
 
-// string, number, boolean
+// ----------------------------------------------- Union
+let userID: string | number = "abc1";
+userID = 123;
+
+// ----------------------------------------------- Object
+let user: { name: string; age: number; isAdmin: boolean; id: string | number };
+
+user = {
+  name: "Blaise",
+  age: 12,
+  isAdmin: true,
+  id: 45678,
+};
+
+// ----------------------------------------------- Array
