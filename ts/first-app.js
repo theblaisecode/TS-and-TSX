46a9e78b8bde9a6936dffc5c694b903941d9dd98
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-02-11 21:40:43 +0100

    first commit

--- ts/first-app.js
+++ ts/first-app.js
@@ -1,42 +1,103 @@
-"use strict";
+var __assign =
+  (this && this.__assign) ||
+  function () {
+    __assign =
+      Object.assign ||
+      function (t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+          s = arguments[i];
+          for (var p in s)
+            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+      };
+    return __assign.apply(this, arguments);
+  };
 // ----------------------------------------------- String
-let userName = "Blaise";
+var userName = "Blaise";
 // userName = 34;
 userName = "Max";
 // ----------------------------------------------- Number
-let userAge = 34;
+var userAge = 34;
 // ----------------------------------------------- Boolean
-let isInvalid = true;
+var isInvalid = true;
 // ----------------------------------------------- Union
-let userID = "abc1";
+var userID = "abc1";
 userID = 123;
 // ----------------------------------------------- Object
-let user;
+var user;
 user = {
-    name: "Blaise",
-    age: 12,
-    isAdmin: true,
-    id: 45678,
+  name: "Blaise",
+  age: 12,
+  isAdmin: true,
+  id: 45678,
 };
 // ----------------------------------------------- Array
-let hobbies;
+var hobbies;
 hobbies = ["Traveling", "Manga", "Drawing"];
 // number[], boolean[], (string | number)[]
 // {name: string; age: number; hobbies: string[]; isAdmin: boolean}[]
 // ----------------------------------------------- Adding types to function parmeter
 function add(a, b) {
-    const result = a + b;
-    return result;
+  var result = a + b;
+  return result;
 }
 function sub(a, b) {
-    const result = a - b;
-    return result;
+  var result = a - b;
+  return result;
 }
 console.log(add(2, 5));
 console.log(sub(5, 2));
 // ----------------------------------------------- Defining function types
 function calculateSum(a, b, calcFn) {
-    calcFn(a, b);
+  calcFn(a, b);
 }
 console.log(calculateSum(2, 5, add));
 console.log(calculateSum(12, 5, sub));
+function calculateSum2(a, b, calcFn) {
+  calcFn(a, b);
+}
+var user2;
+var creds;
+creds = {
+  username: "Blaise",
+  email: "theblaisecode@gmail.com",
+  password: "1234Blaise",
+};
+var newUser = {
+  permission: ["login", "logout"],
+  username: "Blaise",
+};
+var newUser2 = {
+  permission: ["login", "logout"],
+  username: "Blaise",
+  email: "theblaisecode@gmail.com",
+};
+// ----------------------------------------------- Literal Types
+var role;
+role = 1;
+role = "admin";
+role = "editor";
+role = "user";
+function performAction(action, role) {
+  if (role === "admin" && typeof action === "number") {
+    console.log("olla");
+  }
+}
+var textStorage = {
+  storage: [],
+  add: function (data) {
+    this.storage.push(data);
+  },
+};
+var userStorage = {
+  storage: [],
+  add: function (user) {},
+};
+// ------------ creating custom generic functions
+function merge(a, b) {
+  return __assign(__assign({}, a), b);
+}
+var newestUser = merge({ name: "Blaise" }, { age: 12 });
+newestUser.age;
+newestUser.name;
