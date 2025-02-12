5813101a8a460e22a8ed54dfbd0d1d15ff70c0a3
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-02-12 09:55:00 +0100

    first commit

--- react_tsx/src/App.tsx
+++ react_tsx/src/App.tsx
@@ -1,10 +1,28 @@
 import CourseGoal from "./components/CourseGoal";
+import Header from "./components/Header";
+import goalsImg from "./assets/goals.jpg";
 
 function App() {
   return (
-    <>
-      <CourseGoal />
-    </>
+    <main>
+      {/* <CourseGoal
+        title="Learn React + TS"
+        description="Learn it from the ground up!"
+      /> */}
+
+      {/* ------------------------------------------------ */}
+      {/* <Header source={goalsImg} imgAlt="A list of goals" >
+        <h1>Your Course Goals</h1>
+      </Header> */}
+
+      <Header image={{ source: goalsImg, imgAlt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+
+      <CourseGoal title="Olla" description="This is Spanish">
+        <p>I spoke Spanish</p>
+      </CourseGoal>
+    </main>
   );
 }
 
--- react_tsx/src/components/CourseGoal.tsx
+++ react_tsx/src/components/CourseGoal.tsx
@@ -1,14 +1,81 @@
-function CourseGoal() {
+// import { FC } from "react";
+
+// type Goal = {
+//   title: string;
+//   description: string;
+// };
+
+// function CourseGoal(props: Goal) {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{props.title}</h2>
+//         <p>{props.description}</p>
+//       </div>
+
+//       <button>Delete</button>
+//     </article>
+//   );
+// }
+
+// const CourseGoal: FC<Goal> = ({ title, description }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         <p>{description}</p>
+//       </div>
+
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
+
+// ---------------------------------------------
+import { type FC, type PropsWithChildren, type ReactNode } from "react";
+
+// interface Goal {
+//   title: string;
+//   description: string;
+//   children: ReactNode;
+// }
+
+// OR
+
+type Goal = PropsWithChildren<{ title: string; description: string }>;
+
+// function CourseGoal({ title, description, children }: Goal) {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         <p>{description}</p>
+//       </div>
+
+//       {children}
+
+//       <button>Delete</button>
+//     </article>
+//   );
+// }
+
+// OR
+
+const CourseGoal: FC<Goal> = ({ title, description, children }) => {
   return (
     <article>
       <div>
-        <h2>TITLE</h2>
-        <p>DESCRIPTION</p>
+        <h2>{title}</h2>
+        <p>{description}</p>
       </div>
+
+      {children}
 
       <button>Delete</button>
     </article>
   );
-}
+};
 
 export default CourseGoal;
