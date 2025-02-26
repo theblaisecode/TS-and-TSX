311f73ad802dfc5859103e3fdc01fb0bb0eb974b
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-02-26 09:36:56 +0100

    first commit

--- react_tsx/src/components/CourseGoalList.tsx
+++ react_tsx/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import { CourseGoalArr } from "../types/types.tsx";
 import CourseGoal from "./CourseGoal.tsx";
+import InfoBox from "./InfoBox.tsx";
+import { type ReactNode } from "react";
 
 type CourseGoalListProp = {
   goals: CourseGoalArr[];
@@ -7,23 +9,45 @@ type CourseGoalListProp = {
 };
 
 function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProp) {
-  return (
-    <ul>
-      {goals.map((item) => {
-        const { id, title, description } = item;
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
 
-        return (
-          <li key={id}>
-            <CourseGoal
-              id={id}
-              title={title}
-              description={description}
-              onDelete={onDeleteGoal}
-            />
-          </li>
-        );
-      })}
-    </ul>
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You're collecting a lot of goals.Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
+  return (
+    <>
+      {warningBox}
+
+      <ul>
+        {goals.map((item) => {
+          const { id, title, description } = item;
+
+          return (
+            <li key={id}>
+              <CourseGoal
+                id={id}
+                title={title}
+                description={description}
+                onDelete={onDeleteGoal}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
---
+++ react_tsx/src/components/InfoBox.tsx
@@ -0,0 +1,25 @@
+import { type ReactNode } from "react";
+
+type InfoBoxProps = {
+  mode: "hint" | "warning";
+  children: ReactNode;
+};
+
+function InfoBox({ mode, children }: InfoBoxProps) {
+  if (mode === "hint") {
+    return (
+      <aside className="infobox infobox-hint">
+        <p>{children}</p>
+      </aside>
+    );
+  }
+
+  return (
+    <aside className="infobox infobox-warning">
+      <h2>Warning</h2>
+      <p>{children}</p>
+    </aside>
+  );
+}
+
+export default InfoBox;
